1ea58417c1a93dd10fb7fca4f29260c5136b6479
Egor-Cherepanov <105498881+Egor-Cherepanov@users.noreply.github.com>
2024-12-05 13:55:46 +0300

    createElement

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,58 +2,58 @@ import logo from './logo.svg';
 import './App.css';
 import { createElement } from 'react';
 
-export const App = () => {
-	const currentYear = `Current year: ${new Date().getFullYear()}`; //декларативный стиль
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				{/* декларативный стиль */}
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{currentYear}</p>
-				{/* декларативный стиль */}
-			</header>
-		</div>
-	);
-};
-
 // export const App = () => {
 // 	const currentYear = `Current year: ${new Date().getFullYear()}`; //декларативный стиль
-// 	return createElement(
-// 		'div',
-// 		{ className: 'App' },
-// 		createElement(
-// 			'header',
-// 			{ className: 'App-header' },
-// 			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-// 			createElement(
-// 				'p',
-// 				{},
-// 				'Edit',
-// 				createElement('code', {}, 'src/App.js'),
-// 				' and save to reload.',
-// 			),
-// 			createElement(
-// 				'a',
-// 				{
-// 					className: 'App-link',
-// 					href: 'https://reactjs.org',
-// 					target: '_blank',
-// 					rel: 'noopener noreferrer',
-// 				},
-// 				'Learn React',
-// 			),
-// 			createElement('p', {}, currentYear),
-// 		),
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				{/* декларативный стиль */}
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to reload.
+// 				</p>
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+// 				<p>{currentYear}</p>
+// 				{/* декларативный стиль */}
+// 			</header>
+// 		</div>
 // 	);
 // };
+
+export const App = () => {
+	const currentYear = `Current year: ${new Date().getFullYear()}`; //декларативный стиль
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement(
+				'p',
+				{},
+				'Edit',
+				createElement('code', {}, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p', {}, currentYear),
+		),
+	);
+};
